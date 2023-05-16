7f1fdc40eeac7372428a42c46bffbeb4b988090e
athiecheng <72422874+athiecheng@users.noreply.github.com>
2023-05-15 21:04:59 -0700

    implement submission repository

--- Antra_Projects/HRM/Recruiting/ApplicationCore/Contracts/Repositories/ISubmissionRepository.cs
+++ Antra_Projects/HRM/Recruiting/ApplicationCore/Contracts/Repositories/ISubmissionRepository.cs
@@ -5,4 +5,7 @@ namespace ApplicationCore.Contracts.Repositories;
 public interface ISubmissionRepository: IRepository<Submission>
 {
     Task<Submission> GetSubmissionById(int id);
+    Task<List<Submission>> GetAllSubmission();
+    Task<List<Submission>> GetSubmissionsByCandidate(int candidateId);
+    Task<List<Submission>> GetSubmissionsByJob(int jobId);
 }
--- Antra_Projects/HRM/Recruiting/Infrastructures/Repositories/SubmissionRepository.cs
+++ Antra_Projects/HRM/Recruiting/Infrastructures/Repositories/SubmissionRepository.cs
@@ -1,17 +1,39 @@
 using ApplicationCore.Contracts.Repositories;
 using ApplicationCore.Entities;
 using ClassLibrary1.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClassLibrary1.Repositories;
 
 public class SubmissionRepository : Repository<Submission>, ISubmissionRepository
 {
-    public Task<Submission> GetSubmissionById(int id)
+    private readonly RecruitingDbContext _recruitingDbContext;
+    public async Task<Submission> GetSubmissionById(int id)
     {
-        throw new NotImplementedException();
+        var submission = await _recruitingDbContext.Submissions.FirstOrDefaultAsync(s => s.Id == id);
+        return submission;
     }
 
-    public SubmissionRepository(RecruitingDbContext dbContext) : base(dbContext)
+    public async Task<List<Submission>> GetAllSubmission()
     {
+        var submissions = await _recruitingDbContext.Submissions.ToListAsync();
+        return submissions;
+    }
+
+    public async Task<List<Submission>> GetSubmissionsByCandidate(int candidateId)
+    {
+        var submissions = await _recruitingDbContext.Submissions.Where(s => s.CandidateId == candidateId).ToListAsync();
+        return submissions;
+    }
+
+    public async Task<List<Submission>> GetSubmissionsByJob(int jobId)
+    {
+        var submissions = await _recruitingDbContext.Submissions.Where(s => s.JobId == jobId).ToListAsync();
+        return submissions;
+    }
+
+    public SubmissionRepository(RecruitingDbContext dbContext, RecruitingDbContext recruitingDbContext) : base(dbContext)
+    {
+        _recruitingDbContext = recruitingDbContext;
     }
 }
